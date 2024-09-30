02e7f2b2fcacd61f1ef3faf12bb203b3f6429b9a
DBelen99 <daysib.anator@educa.jcyl.es>
2024-09-30 18:28:33 +0200

    Ejercicio1 hecho

---
+++ src/main/java/org/example/Ejercicio1.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class Ejercicio1 {
+    //Muestra los números del 320 al 160, contando de 20 en 20 hacia atrás.
+    public static void main(String[] args) {
+
+        int numResta=320;
+
+        while(numResta>=160){
+            System.out.println(numResta);
+            numResta-=20;
+        }
+    }
+
+
+}
